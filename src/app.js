b11ca20194b03fbae33f1e2b07edfb8c1098637f
Jnv821 <jnv821@gmail.com>
2022-11-18 19:31:34 +0000

    Uploaded the code

--- src/app.js
+++ src/app.js
@@ -7,5 +7,41 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  let pronouns = ["the", "our", "my"];
+  let adjs = ["great", "big", "small", "amazing"];
+  let nouns = ["jogger", "racoon", "runner", "walker"];
+  let domains = ["com", "us", "es"];
+
+  let name = "";
+
+  let a = "bob";
+  let b = "amazing";
+  let c = "flies";
+
+  // Using for of*
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let domain of domains) {
+          name = `${pronoun}${adj}${noun}.${domain}`;
+          console.log(name);
+        }
+      }
+    }
+  }
+
+  /* Using classic fors
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjs.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let l = 0; l < domains.length; l++) {
+          name = `${pronouns[i]}${adjs[j]}${nouns[k]}.${domains[l]}`;
+          console.log(name);
+        }
+      }
+    }
+  }
+*/
   console.log("Hello Rigo from the console!");
 };
